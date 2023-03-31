0268f1fdec4ef0d368595bfd72fc727836a8ae67
Pritesh <priteshwani5@gmail.com>
2023-03-31 23:32:10 +0530

    day2 "done"

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 
 //STLE Enter
 class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Pritesh";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Catlog App"),
+      ),
       body: Center(
           child: Container(
-        child: Text("Welcome pritesh wani"),
+        child: Text("Welcome to $days and $name"),
       )),
       drawer: Drawer(),
     );
